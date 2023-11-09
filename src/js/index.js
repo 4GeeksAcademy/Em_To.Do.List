4f83f995aa141d58f45e785418a34c3a355f2ac2
Emilio <136965828+Emdihoy@users.noreply.github.com>
2023-11-09 16:41:21 +0000

    Changes: Functions are set, useStates created, styles are yet to be created.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 //import react into the bundle
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
@@ -7,6 +7,9 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import ToDoList from "./component/toDoList.jsx";
+
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<ToDoList />, document.querySelector("#app"));
